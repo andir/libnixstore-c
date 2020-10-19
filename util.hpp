bc7319d0812deab32a9d0d124261111cfe7cbaeb
Andreas Rammhold <andreas@rammhold.de>
2020-10-19 12:07:16 +0200

    Reformat util.hpp

--- util.hpp
+++ util.hpp
@@ -1,32 +1,30 @@
-#include <string>
 #include <sstream>
-
+#include <string>
 
 namespace util {
-	template<typename T, typename S>
-	std::string join(T &iterable, const S& separator) {
-		std::stringstream ss;
+template <typename T, typename S>
+std::string join(T &iterable, const S &separator) {
+  std::stringstream ss;
 
-		bool first = true;
-		for (const auto& e : iterable) {
-			if (!first) {
-				ss << separator;
-			} else {
-				first = false;
-			}
-			ss << e;
-		}
+  bool first = true;
+  for (const auto &e : iterable) {
+    if (!first) {
+      ss << separator;
+    } else {
+      first = false;
+    }
+    ss << e;
+  }
 
-		return ss.str();
-	}
-
-
-	template<typename F>
-	class OnScopeExit {
-		private:
-			F f;
-		public:
-		OnScopeExit(F f) : f(f) {}
-		~OnScopeExit() { f(); }
-	};
+  return ss.str();
 }
+
+template <typename F> class OnScopeExit {
+private:
+  F f;
+
+public:
+  OnScopeExit(F f) : f(f) {}
+  ~OnScopeExit() { f(); }
+};
+} // namespace util
